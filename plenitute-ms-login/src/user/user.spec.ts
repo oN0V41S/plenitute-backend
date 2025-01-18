edad289f664e50d11b67ed2ccac4091b3a8a8fcd
oN0V41S <rafaelaugustonnovais@gmail.com>
2025-01-17 22:39:46 -0300

    feature: add unit tests for UserService and UserController

--- plenitute-ms-login/src/user/user.spec.ts
+++ plenitute-ms-login/src/user/user.spec.ts
@@ -2,20 +2,32 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { CreateUserRequest } from './user-request.dto';
+import { ClientProxy } from '@nestjs/microservices';
+import { of } from 'rxjs';
 
 // Testing User Resources: Controller and Services
 describe('Testing Controller && Testing Services -- User', () => {
   let controller: UserController;
   let service: UserService;
+  let backendClient: ClientProxy;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: 'backend',
+          useValue: {
+            send: jest.fn().mockReturnValue(of({ token: 'generated_token' })),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
     service = module.get<UserService>(UserService);
+    backendClient = module.get<ClientProxy>('backend');
   });
 
   describe('UserController', () => {
@@ -33,21 +45,23 @@ describe('Testing Controller && Testing Services -- User', () => {
         const result = await controller.login(createUserRequest);
         expect(result).toEqual({ token: 'generated_token' });
         expect(service.login).toHaveBeenCalledWith(createUserRequest);
+        expect(backendClient.send).toHaveBeenCalledWith('login_authenticate', createUserRequest);
       });
     });
   });
 
-//   describe('UserService', () => {
-//     describe('login', () => {
-//       it('should generate a token', async () => {
-//         const createUserRequest: CreateUserRequest = {
-//           email: 'sucess',
-//           password: 'sucess',
-//         };
+  describe('UserService', () => {
+    describe('login', () => {
+      it('should generate a token', async () => {
+        const createUserRequest: CreateUserRequest = {
+          email: 'sucess',
+          password: 'sucess',
+        };
 
-//         const result = await service.login(createUserRequest);
-//         expect(result).toEqual({ token: 'generated_token' });
-//       });
-//     });
-//   });
+        const result = await service.login(createUserRequest);
+        expect(result).toEqual({ token: 'generated_token' });
+      });
+    });
+  });
 });
+
